e794f58cb352c0b5b8a42e946edfd0c6c07a3f66
ab77 <anton@balena.io>
2021-08-13 09:32:09 -0700

    Add missing init script
    * balena-root-ca
    
    Change-type: patch

--- src/confd-entry.sh
+++ src/confd-entry.sh
@@ -14,9 +14,11 @@ if [[ $USE_CONFD -eq 1 ]]; then
   set -a; [ -f /usr/src/app/config/env ] && source /usr/src/app/config/env; set +a
 fi
 
-/usr/bin/configure-balena-host-envvars.sh
+[ -f /usr/bin/configure-balena-host-envvars.sh ] \
+  && /usr/bin/configure-balena-host-envvars.sh
 
-/usr/bin/configure-balena-root-ca.sh
+[ -f /usr/bin/configure-balena-root-ca.sh ] \
+  && /usr/bin/configure-balena-root-ca.sh
 
 set -a; [ -f /etc/docker.env ] && source /etc/docker.env; set +a
 
