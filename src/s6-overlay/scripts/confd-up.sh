b22260970564f73078a50ac25549d08fc8802b59
Kyle Harding <kyle@balena.io>
2025-09-30 09:17:10 -0400

    Disable s6-log blocking mode
    From the s6-log docs: https://skarnet.org/software/s6/s6-log.html
    
    > By default, s6-log keeps reading from stdin even if its buffers
    > still contain data. -b is safer, but may slow down your service;
    > the default is faster, but may lead to unbound memory use if you
    > have a lot of output to write to a slow file system.
    
    Change-type: patch
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- src/s6-overlay/scripts/confd-up.sh
+++ src/s6-overlay/scripts/confd-up.sh
@@ -2,7 +2,7 @@
 # shellcheck shell=bash
 
 # Redirect all future stdout/stderr to s6-log
-exec > >(exec s6-log -b p"confd[$$]:" 1 || true) 2>&1
+exec > >(exec s6-log p"confd[$$]:" 1 || true) 2>&1
 
 # Populate /etc/docker.env with all container environment variables
 for var in $(compgen -e); do
